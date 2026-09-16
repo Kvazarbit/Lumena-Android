ee8fe84ef4457a12d9267f4c810f7c0b91e0db73
Kvazar <46287097+Kvazarbit@users.noreply.github.com>
2026-09-16 22:33:18 +0200

    Set version 0.7.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lumena.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.7.1"
+        versionCode = 9
+        versionName = "0.7.2"
     }
 
     compileOptions {
