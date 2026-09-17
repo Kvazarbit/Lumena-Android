50edfd6931e888fa358f9a44c7df5efa9a3e9793
Kvazar <46287097+Kvazarbit@users.noreply.github.com>
2026-09-17 12:58:54 +0200

    Set version 0.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lumena.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.7.3"
+        versionCode = 11
+        versionName = "0.8.0"
     }
 
     compileOptions {
