4ed4d524520bff47f6747a9f8e7c5739c5c25d2c
Kvazar <46287097+Kvazarbit@users.noreply.github.com>
2026-09-17 19:54:59 +0200

    Bump Lumena to 0.9.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lumena.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.9.1"
+        versionCode = 15
+        versionName = "0.9.2"
     }
 
     compileOptions {
