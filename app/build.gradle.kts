54a1d34e9afb58201cbd6a77df375b12054e87b0
Kvazar <46287097+Kvazarbit@users.noreply.github.com>
2026-09-17 18:56:11 +0200

    Bump app version to 0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lumena.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.8.1"
+        versionCode = 13
+        versionName = "0.9.0"
     }
 
     compileOptions {
